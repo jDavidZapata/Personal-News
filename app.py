436ca2efbd9b105777f87a5afb2d8ab26fba84db
Juan David Zapata <jdavidzapata1983@gmail.com>
2019-01-02 21:18:34 -0500

    Update

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort, redirect, url_for, g, jsonify
+import os, requests, json
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
+
+app.secret_key = b'_3#y2L"F4Q8z\n\xec]/'
+
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    error = None
