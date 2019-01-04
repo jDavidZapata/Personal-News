277ca230702bf99ffab29475d402344c593898bc
Juan David Zapata <jdavidzapata1983@gmail.com>
2019-01-03 23:44:07 -0500

    Update

--- app.py
+++ app.py
@@ -37,4 +37,83 @@ def index():
         return render_template("index.html", error=error)
 
     else:
+        g.user = db.execute(
+            'SELECT * FROM users WHERE id IN (:id)', {"id": user_id, }
+        ).fetchone()
+        
         return render_template("index.html", error=error)
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    """Register a new user.
+        Check if the user is in session.
+        Validates that the name and email are not already taken. 
+        Redirect to index page.
+        #Hashes the password for security.
+    """
+    error = None
+
+    success = None
+
+    user_id = session.get('user_id')
+
+    if ('user_id' in session):
+
+        g.user = db.execute(
+            'SELECT * FROM users WHERE id IN (:id)', {"id": user_id, }
+        ).fetchone()
+
+        # change to redirect to current page
+        return render_template("index.html")
+
+    if request.method == 'POST':
+
+        name = request.form['name']
+        password = request.form['password']
+        email = request.form['email']
+
+        error = None
+
+        if not name:
+            error = 'Name is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif not email:
+            error = 'Email required.'
+
+        user = db.execute("SELECT * FROM users WHERE email IN (:email)",
+                          {"email": email}).fetchone()
+
+        if user is not None:
+            error = 'User with email {0} is already registered.'.format(email)
+
+        if error is None:
+            """If the name is available, store it in the database and go to the login page"""
+
+            db.execute("INSERT INTO users (name, password, email) VALUES (:name, :password, :email)",
+                       {"name": name, "password": password, "email": email})
+            db.commit()
+
+            user = db.execute("SELECT * FROM users WHERE email IN (:email)", {
+                              "email": email}).fetchone()
+
+            """Store the user id in a new session and return to the index"""
+
+            session.clear()
+            session['user_id'] = user['id']
+            g.user = user
+
+            '''
+            g.user = db.execute(
+                'SELECT * FROM users WHERE id IN (:id)', {"id": user_id, }
+            ).fetchone()
+            '''
+
+            success = 'Thank You For Signing Up.'
+
+            # Change to redirect to curent page
+            return redirect(url_for('index'))
+
+    return render_template('auth/register.html', book=book, error=error, success=success)
+
